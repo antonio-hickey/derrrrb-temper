ee73a7f23648dd623dff0bac91a0c896a259b058
Antonio Hickey <antoniohickey99@gmail.com>
2023-05-16 19:46:23 -0400

    raw infrared sesnsooooor working

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,25 @@
 #![no_main]
 
 use panic_halt as _;
+use mlx9061x::{Mlx9061x, SlaveAddr};
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    // Define peripherals (digital pins)
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let addr = SlaveAddr::default();
+    let mut i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(),
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
 
-    // Digital pin 13 is the LED marked "L" on my board
-    let mut led = pins.d13.into_output();
+    let mut sensor = Mlx9061x::new_mlx90614(i2c, addr, 5).unwrap(); 
 
-    // Tell the board to set led pin to high volatage (pulled to supply voltage)
-    led.set_high(); 
-
-    // Toggle led, to prove the program is running on the board
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(10000);
-        led.toggle();
-        arduino_hal::delay_ms(1000);
-        led.toggle();
-        arduino_hal::delay_ms(1000);
-        led.toggle();
-        arduino_hal::delay_ms(8000);
+        let t = sensor.raw_ir_channel1().unwrap();
+        ufmt::uwriteln!(&mut serial, "temp: {}", t).unwrap();
     }
 }
